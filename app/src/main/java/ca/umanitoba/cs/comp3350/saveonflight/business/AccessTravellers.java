e278ff5d83663a1e9db32c6067973d360aa0c8a4
Kenny Zhang <zhang-youtian@hotmail.com>
2017-05-24 16:51:43 -0500

    Provide implementation for AccessTravellers class

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/AccessTravellers.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/AccessTravellers.java
@@ -1,8 +1,60 @@
 package ca.umanitoba.cs.comp3350.saveonflight.business;
 
-/**
- * Created by zhang on 2017-05-24.
- */
+import java.util.List;
+
+import ca.umanitoba.cs.comp3350.saveonflight.application.Main;
+import ca.umanitoba.cs.comp3350.saveonflight.application.Services;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.Traveller;
+import ca.umanitoba.cs.comp3350.saveonflight.persistence.DataAccessStub;
 
 public class AccessTravellers {
+    private DataAccessStub dataAccess;
+    private List<Traveller> travellers;
+
+    // For iterating through all Travellers
+    private Traveller currentTraveller;
+    private int currentIndex;
+
+    public AccessTravellers() {
+        dataAccess = Services.getDataAccess(Main.DB_NAME);
+        travellers = null;
+        currentTraveller = null;
+        currentIndex = 0;
+    }
+
+    public List<Traveller> getTravellers() {
+        return dataAccess.getTravellers();
+    }
+
+    public Traveller getNext() {
+        if (travellers == null) {
+            travellers = dataAccess.getTravellers();
+            currentIndex = 0;
+        }
+
+        if (currentIndex < travellers.size()) {
+            currentTraveller = travellers.get(currentIndex);
+            currentIndex++;
+        }
+        else {
+            travellers = null;
+            currentTraveller = null;
+            currentIndex = 0;
+        }
+
+        return currentTraveller;
+    }
+
+    public boolean insertTravller(Traveller traveller) {
+        return dataAccess.insertTraveller(traveller);
+    }
+
+    public boolean updateTraveller() {
+        // TODO
+        return false;
+    }
+
+    public boolean deleteTraveller(Traveller traveller) {
+        return dataAccess.deleteTraveller(traveller);
+    }
 }
