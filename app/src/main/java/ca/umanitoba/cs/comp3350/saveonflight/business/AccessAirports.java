b179c8e276f2a5d62c605552a5124ec886617bb8
Shenyun Wang <wangs321@myumanitoba.ca>
2017-05-25 14:50:13 -0500

    access airports

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/AccessAirports.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/AccessAirports.java
@@ -1,5 +1,34 @@
 package ca.umanitoba.cs.comp3350.saveonflight.business;
 
-public class AccessAirports {
+import java.util.List;
 
+import ca.umanitoba.cs.comp3350.saveonflight.application.Main;
+import ca.umanitoba.cs.comp3350.saveonflight.application.Services;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.Airport;
+import ca.umanitoba.cs.comp3350.saveonflight.persistence.DataAccessStub;
+//get airlines in an airport
+public class AccessAirports {
+    private DataAccessStub dataAccess;
+    private List<Airport> airports;
+
+
+    AccessAirports(){
+        dataAccess = (DataAccessStub) Services.getDataAccess(Main.DB_NAME);
+        airports =  null;
+    }
+
+    public List<Airport> getAirports(){
+        return dataAccess.getAirports();
+    }
+
+    public boolean addAirport(Airport a){
+        return dataAccess.insertAirport(a);
+    }
+
+    public Airport deleteAirport(Airport a){
+        Airport deleted = null;
+        if(dataAccess.deleteAirport(a))
+            deleted = a;
+        return deleted;
+    }
 }
