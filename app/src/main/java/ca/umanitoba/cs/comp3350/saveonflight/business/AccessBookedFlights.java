7eff366efebf9c2f32f40445fd259b899f35be2f
Shenyun Wang <wangs321@myumanitoba.ca>
2017-05-25 14:32:16 -0500

    accessbookedflights

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/AccessBookedFlights.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/AccessBookedFlights.java
@@ -1,8 +1,44 @@
 package ca.umanitoba.cs.comp3350.saveonflight.business;
 
+import java.util.List;
+
+import ca.umanitoba.cs.comp3350.saveonflight.application.Main;
+import ca.umanitoba.cs.comp3350.saveonflight.application.Services;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.BookedFlight;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.Flight;
+import ca.umanitoba.cs.comp3350.saveonflight.persistence.DataAccessStub;
+
 /**
  * Created by zhang on 2017-05-24.
  */
 
 public class AccessBookedFlights {
+    private DataAccessStub dataAccess;
+    private List<BookedFlight> bookedFlights;
+    private Flight flight;
+    private int currentFlight;
+
+    public AccessBookedFlights(){
+        dataAccess = (DataAccessStub) Services.getDataAccess(Main.DB_NAME);
+        bookedFlights = null;
+        flight = null;
+        currentFlight = 0;
+    }
+
+    public List<BookedFlight> getFlights(){
+        return dataAccess.getBookedFlights();
+    }
+
+    public boolean addBookedFlight(BookedFlight f){
+        return dataAccess.insertBookedFlight(f);
+    }
+
+
+    public BookedFlight deleteFlight(BookedFlight f){
+        BookedFlight deleted = null;
+        if(dataAccess.deleteBookedFlight(f))
+            deleted = f;
+        return deleted;
+    }
 }
+
