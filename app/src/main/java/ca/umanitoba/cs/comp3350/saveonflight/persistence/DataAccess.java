8ad0ca9e9230615d982350a359a0b2d56f05cc41
longyu <dagoulixiaobaozi@gmail.com>
2017-05-29 15:25:48 -0500

    DataAccess interface done

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/persistence/DataAccess.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/persistence/DataAccess.java
@@ -1,8 +1,78 @@
 package ca.umanitoba.cs.comp3350.saveonflight.persistence;
 
+import java.util.ArrayList;
+
+import ca.umanitoba.cs.comp3350.saveonflight.objects.Airline;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.Airport;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.BookedFlight;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.Flight;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.Traveller;
+
 /**
  * Created by zhang on 2017-05-25.
  */
 
 interface DataAccess {
+
+    public void open(String dbName);
+        // TODO: Populate the 5 "tables" (ArrayLists) with fake data
+
+    public void close();
+
+    // AIRLINE TABLE METHDOS
+    public ArrayList<Airline> getAirlines();
+        // TODO: return all airlines
+
+    public boolean insertAirline(Airline airline);
+
+    // TODO: public ArrayList<Student> getStudentRandom(Student currentStudent)???
+
+    public boolean updateAirline(Airline airline);
+        // TODO: Not sure what the method signature for these update methods should be - kenny
+
+    public boolean deleteAirline(Airline CurrectAirline);
+        // TODO: delete the specified airline from the Airlines table
+
+    // AIRPORT TABLE METHODS
+    public ArrayList<Airport> getAirports();
+        // TODO: return ArrayList of all airports
+
+    public boolean insertAirport(Airport airport);
+
+    public boolean updateAirport();
+
+    public boolean deleteAirport(Airport airport);
+
+
+    // BOOKEDFLIGHTS TABLE METHODS
+    public ArrayList<BookedFlight> getBookedFlights();
+
+    public boolean insertBookedFlight(BookedFlight bookedFlight);
+
+    public boolean updateBookedFlight();
+
+    public boolean deleteBookedFlight(BookedFlight bookedFlight);
+
+    public ArrayList<BookedFlight> getTravellersBookedFlights(Traveller traveller);
+
+    public ArrayList<BookedFlight> getTravellersOnFlight(Flight flight);
+
+    // FLIGHTS TABLE METHODS
+    public ArrayList<Flight> getFlights();
+
+    public boolean insertFlight(Flight flight);
+
+    public boolean updateFlight();
+
+    public boolean deleteFlight(Flight flight);
+
+
+    // TRAVELLERS TABLE METHODS
+    public ArrayList<Traveller> getTravellers();
+    public boolean insertTraveller(Traveller traveller);
+
+    public boolean updateTraveller();
+
+    public boolean deleteTraveller(Traveller traveller);
+
 }
