3073dcf26a82f6eec03c9fce34a9e8fed1f60790
Andy Lun <andylun1206@gmail.com>
2017-06-01 01:30:09 -0500

    Refactoring find flight interface and implementation to lessen coupling

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/FindFlights.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/business/FindFlights.java
@@ -6,12 +6,12 @@ import java.util.List;
 import ca.umanitoba.cs.comp3350.saveonflight.objects.Airline;
 import ca.umanitoba.cs.comp3350.saveonflight.objects.Airport;
 import ca.umanitoba.cs.comp3350.saveonflight.objects.Flight;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.SearchCriteria;
 
-interface FindFlights {
+public interface FindFlights {
     List<Flight> getAllFlights();
 
     List<Flight> search(Airline airline, Date departureDate, Airport departs, Airport arrives);
 
-    List<Flight> search(Airport departs, Airport arrives, Date departureDate, Date returnDate,
-                        int numTravellers, int maxPrice, Airline airline, Flight.FlightClass flightClass);
+    List<Flight> search(SearchCriteria criteria);
 }
