1e9d692974ec9d11837b1b7f9da1688b659619e7
Andy Lun <andylun1206@gmail.com>
2017-06-01 01:25:48 -0500

    Refactoring database mapped objects to implement Parcelable

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/objects/Airline.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/objects/Airline.java
@@ -1,6 +1,17 @@
 package ca.umanitoba.cs.comp3350.saveonflight.objects;
 
-public class Airline {
+/**
+ * Airline.java
+ *
+ * Object mapped to the airline table DB
+ *
+ * @author Andy Lun
+ */
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Airline implements Parcelable {
     private String name;
 
     public Airline(String name) {
@@ -30,5 +41,30 @@ public class Airline {
         }
 
         return result;
+    }
+    
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(name);
+    }
+    
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        @Override
+        public Airline[] newArray(int size) {
+            return new Airline[size];
+        }
+        
+        @Override
+        public Airline createFromParcel(Parcel in) {
+            return new Airline(in);
+        }
+    };
+    
+    private Airline(Parcel in) {
+        name = in.readString();
     }
 }
