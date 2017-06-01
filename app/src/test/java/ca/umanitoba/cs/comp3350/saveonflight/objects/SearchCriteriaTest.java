aaee83f582c365910c225ed47a2b9603fd621b40
Kenny Zhang <busstopbench@gmail.com>
2017-06-01 11:39:18 -0500

    Create class for SearchCriteria test

---
+++ app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/objects/SearchCriteriaTest.java
@@ -0,0 +1,24 @@
+package ca.umanitoba.cs.comp3350.saveonflight.objects;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchCriteriaTest {
+    private SearchCriteria searchCriteria;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testMatches() {
+
+    }
+}
