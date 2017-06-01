d132ba9accc28121effb355fc958bb418386f17e
Andy Lun <andylun1206@gmail.com>
2017-06-01 01:28:56 -0500

    Refactoring listview object for search page

---
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/objects/SearchCriteriaListView.java
@@ -0,0 +1,25 @@
+package ca.umanitoba.cs.comp3350.saveonflight.objects;
+
+/**
+ * SearchCriteriaListView.java
+ *
+ * Object representing an entry in the listview on the search page.
+ *
+ * @author Andy Lun
+ */
+
+public class SearchCriteriaListView {
+	private int icon;
+	private String title;
+	
+	public SearchCriteriaListView(int icon, String title) {
+		this.icon = icon;
+		this.title = title;
+	}
+	
+	public int getIcon() { return icon; }
+	public void setIcon(int icon) { this.icon = icon; }
+	
+	public String getTitle() { return title; }
+	public void setTitle(String title) { this.title = title; }
+}
