68a37924bb8b47809420fe0cdbac24d5c2c38720
Andy Lun <andylun1206@gmail.com>
2017-06-01 01:41:06 -0500

    Adding supporting functionality for viewing searched flights

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/presentation/SearchCriteriaArrayAdapter.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/presentation/SearchCriteriaArrayAdapter.java
@@ -1,46 +1,117 @@
 package ca.umanitoba.cs.comp3350.saveonflight.presentation;
 
+/**
+ * SearchCriteriaArrayAdapter.java
+ *
+ * ArrayAdapter for SearchCriteria. Used to populate a listview in search.
+ *
+ * @author Andy Lun
+ */
+
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Context;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.*;
 import ca.umanitoba.cs.comp3350.saveonflight.R;
+import ca.umanitoba.cs.comp3350.saveonflight.objects.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class SearchCriteriaArrayAdapter extends ArrayAdapter<SearchCriteria> implements OnDateSetListener {
+public class SearchCriteriaArrayAdapter extends ArrayAdapter<SearchCriteriaListView> implements OnDateSetListener {
 	private final Context context;
 	private final int layoutResourceId;
-	private final ArrayList<SearchCriteria> criterias;
+	private final ArrayList<SearchCriteriaListView> criteriaListList;
+	private static SearchCriteria criteria;
 	
 	private EditText activeDateDisplay;
 	
-	public SearchCriteriaArrayAdapter(Context context, int layoutResourceId, ArrayList<SearchCriteria> criterias) {
-		super(context, layoutResourceId, criterias);
+	public SearchCriteriaArrayAdapter(Context context, int layoutResourceId, ArrayList<SearchCriteriaListView> criteriaListList) {
+		super(context, layoutResourceId, criteriaListList);
 		this.context = context;
 		this.layoutResourceId = layoutResourceId;
-		this.criterias = criterias;
+		this.criteriaListList = criteriaListList;
+		
+		criteria = new SearchCriteria();
 	}
 	
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(final int position, final View convertView, final ViewGroup parent) {
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View row = inflater.inflate(layoutResourceId, parent, false);
-		SearchCriteria criteria = criterias.get(position);
-		((ImageView) row.findViewById(R.id.imageView_search_criteria_icon)).setImageResource(criteria.getIcon());
-		final EditText input = (EditText) row.findViewById(R.id.editText_search_criteria_input);
-		input.setHint(criteria.getTitle());
+		final View row = inflater.inflate(layoutResourceId, parent, false);
+		final SearchCriteriaListView criteriaList = criteriaListList.get(position);
 		
-		switch(criteria.getIcon()) {
+		((ImageView) row.findViewById(R.id.imageView_search_criteria_icon)).setImageResource(criteriaList.getIcon());
+		final EditText input = (EditText) row.findViewById(R.id.editText_search_criteria_input);
+		input.setHint(criteriaList.getTitle());
+		input.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) { }
+			
+			@Override
+			public void onTextChanged(CharSequence charSequence, int start, int before, int count) { }
+			
+			@Override
+			public void afterTextChanged(Editable editable) {
+				String inputText = input.getText().toString().trim();
+				String title = criteriaList.getTitle();
+				
+				if (!inputText.isEmpty()) {
+					if (row.getResources().getString(R.string.search_origin).equals(title)) {
+						criteria.setOrigin(new Airport(inputText));
+					} else if (row.getResources().getString(R.string.search_destination).equals(title)) {
+						criteria.setDestination(new Airport(inputText));
+					} else if (row.getResources().getString(R.string.search_departure_date).equals(title)) {
+						try {
+							SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA);
+							criteria.setDepartureDate(sdf.parse(inputText));
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
+					} else if (row.getResources().getString(R.string.search_return_date).equals(title)) {
+						try {
+							SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA);
+							criteria.setReturnDate(sdf.parse(inputText));
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
+					} else if (row.getResources().getString(R.string.search_num_travellers).equals(title)) {
+						criteria.setNumTravellers(Integer.parseInt(inputText));
+					} else if (row.getResources().getString(R.string.search_max_price).equals(title)) {
+						criteria.setMaxPrice(Double.parseDouble(inputText));
+					} else if (row.getResources().getString(R.string.search_airlines).equals(title)) {
+						criteria.setPreferredAirlines(new Airline(inputText));
+					} else if (row.getResources().getString(R.string.search_class).equals(title)) {
+						criteria.setPreferredClass(Flight.FlightClass.FIRST_CLASS);
+					}
+				}
+			}
+		});
+		
+//		((CheckBox) row.findViewById(R.id.checkBox_search_nonstop)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//			@Override
+//			public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+//				criteria.setNonstop(isChecked);
+//			}
+//		});
+//
+//		((CheckBox) row.findViewById(R.id.checkBox_search_refundable)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//			@Override
+//			public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+//				criteria.setRefundable(isChecked);
+//			}
+//		});
+		
+		switch(criteriaList.getIcon()) {
 			case R.drawable.ic_clock:
 				input.setInputType(InputType.TYPE_CLASS_DATETIME);
 				input.setFocusable(false);
@@ -77,4 +148,8 @@ public class SearchCriteriaArrayAdapter extends ArrayAdapter<SearchCriteria> imp
 		DatePickerDialog dialog = new DatePickerDialog(this.getContext(), this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 		dialog.show();
 	}
+	
+	public static SearchCriteria getCriteria() { return criteria; }
+	public static void resetCriteria() { criteria = new SearchCriteria(); }
+	
 }
