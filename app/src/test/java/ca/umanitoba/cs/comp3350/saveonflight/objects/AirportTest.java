e3a4f1a50a7544a0ca18ebfe699a6133d265c9b7
Shenyun Wang <wangs321@myumanitoba.ca>
2017-06-02 10:45:32 -0500

    airporttest complete

--- app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/objects/AirportTest.java
+++ app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/objects/AirportTest.java
@@ -2,15 +2,28 @@ package ca.umanitoba.cs.comp3350.saveonflight.objects;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.Assert.*;
 
 public class AirportTest {
+    private final Airport AIRPORT1 = new Airport("WPG01");
+    private Airport airport2;
+
     @Before
     public void setUp() {
-
+        airport2 = new Airport("WPG02");
     }
 
     @After
     public void tearDown() {
-
+        airport2 = null;
     }
+
+    @Test
+    public void testEqualsMethod(){
+        assertFalse(AIRPORT1.equals(airport2)); //WPG01 != WPG02
+        airport2.setAirportCode("WPG01");
+        assertTrue(AIRPORT1.equals(airport2)); //WPG01 = WPG01
+    }
+
 }
