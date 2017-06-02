fbbc7ec55cfc9066420315dd2e60153446fa8977
Shenyun Wang <wangs321@myumanitoba.ca>
2017-06-02 10:34:05 -0500

    airlineTest complete

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/objects/Airline.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/objects/Airline.java
@@ -1,6 +1,6 @@
 package ca.umanitoba.cs.comp3350.saveonflight.objects;
 
-/**
+/*
  * Airline.java
  *
  * Object mapped to the airline table DB
@@ -35,7 +35,7 @@ public class Airline implements Parcelable {
 
         if (object instanceof Airline) {
             Airline other = (Airline) object;
-            if (other.name.equals(name)) {
+            if (other.name.equals(this.name)) {
                 result = true;
             }
         }
