31031447554863e8fd6457137146f97a907b26f4
Kenny Zhang <zhang-youtian@hotmail.com>
2017-06-04 08:52:50 -0500

    Update SearchCriteriaTest.java
    Change constant variable names to all caps. Changed testMatches() method to use the constants for testing instead of new variables.

--- app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/objects/SearchCriteriaTest.java
+++ app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/objects/SearchCriteriaTest.java
@@ -15,19 +15,18 @@ public class SearchCriteriaTest {
 
     private final Airport ORIGIN = new Airport("YWG");
     private final Airport DEST = new Airport("YVR");
-    private final Date departureDate = new Date(117,5,6,14,40);
-    private final Date returnDate = new Date(117,5,16,14,40);
-    private final int numTravellers = 3;
+    private final Date DEPARTURE_DATE = new Date(117,5,6,14,40);
+    private final Date RETURN_DATE = new Date(117,5,16,14,40);
+    private final int NUM_TRAVELLERS = 3;
     private final double  MAX = 1000.00;
     private final Airline AIRLINE = new Airline("WestJet", R.mipmap.ic_westjet);
-    private final FlightClassEnum preferredClass = FlightClassEnum.FIRST_CLASS;
-    private final boolean nonstop = true;
-    private final boolean refundable = true;
+    private final FlightClassEnum PREFERRED_CLASS = FlightClassEnum.FIRST_CLASS;
+    private final boolean NONSTOP = true;
+    private final boolean REFUNDABLE = true;
 
     @Before
     public void setUp() {
-
-        searchCriteria = new SearchCriteria(ORIGIN,DEST,departureDate,returnDate,numTravellers,MAX,AIRLINE,preferredClass,nonstop,refundable);
+        searchCriteria = new SearchCriteria(ORIGIN,DEST,DEPARTURE_DATE,RETURN_DATE,NUM_TRAVELLERS,MAX,AIRLINE,PREFERRED_CLASS,NONSTOP,REFUNDABLE);
     }
 
     @After
@@ -37,28 +36,18 @@ public class SearchCriteriaTest {
 
     @Test
     public void testMatches() {
-
-        Airport originText = new Airport("YWG");
-        Airport destText = new Airport("YVR");
-        Date departtureDateTest = new Date(117,5,6,14,40);
-        Date returnDateTest = new Date(117,5,16,14,40);
-        Airline airlineTest = new Airline("WestJet", R.mipmap.ic_westjet);
-        FlightClassEnum preferredClassTest = FlightClassEnum.FIRST_CLASS;
-
         System.out.println("\nStarting test searchCriteria");
 
         assertNotNull(searchCriteria);
-        assertTrue(originText.equals(searchCriteria.getOrigin()));
-        assertTrue(destText.equals(searchCriteria.getDestination()));
-        assertTrue(departtureDateTest.equals(searchCriteria.getDepartureDate()));
-        assertTrue(returnDateTest.equals(searchCriteria.getReturnDate()));
-        assertTrue(3 == searchCriteria.getNumTravellers());
-        assertTrue(1000 == searchCriteria.getMaxPrice());
-        assertTrue(airlineTest.equals(searchCriteria.getPreferredAirlines()));
-        assertTrue(preferredClassTest.equals(searchCriteria.getPreferredClass()));
+        assertTrue(ORIGIN.equals(searchCriteria.getOrigin()));
+        assertTrue(DEST.equals(searchCriteria.getDestination()));
+        assertTrue(DEPARTURE_DATE.equals(searchCriteria.getDepartureDate()));
+        assertTrue(RETURN_DATE.equals(searchCriteria.getReturnDate()));
+        assertTrue(NUM_TRAVELLERS == searchCriteria.getNumTravellers());
+        assertTrue(MAX == searchCriteria.getMaxPrice());
+        assertTrue(AIRLINE.equals(searchCriteria.getPreferredAirlines()));
+        assertTrue(PREFERRED_CLASS.equals(searchCriteria.getPreferredClass()));
         assertTrue(searchCriteria.isNonstop());
         assertTrue(searchCriteria.isRefundable());
-
-
     }
 }
