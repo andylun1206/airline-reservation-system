5e876c544fdd8de37ff65af78e3c34b0bd488e6f
Kenny Zhang <zhang-youtian@hotmail.com>
2017-06-06 14:24:33 -0500

    Add test to AirportTest for contains() method

--- app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/objects/AirportTest.java
+++ app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/objects/AirportTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 import static junit.framework.Assert.*;
 
 public class AirportTest {
-    private final Airport AIRPORT1 = new Airport("WPG01");
+    private final String CODE1 = "WPG01";
+    private final Airport AIRPORT1 = new Airport(CODE1);
     private Airport airport2;
 
     @Before
@@ -21,10 +22,25 @@ public class AirportTest {
     }
 
     @Test
-    public void testEqualsMethod() {
+    public void testEquals() {
+        assertFalse(AIRPORT1.equals(null));
+        assertFalse(AIRPORT1.equals("ABC"));
         assertFalse(AIRPORT1.equals(airport2)); //WPG01 != WPG02
-        airport2.setAirportCode("WPG01");
-        assertTrue(AIRPORT1.equals(airport2)); //WPG01 = WPG01
+        airport2.setAirportCode(CODE1);
+        assertTrue(AIRPORT1.equals(airport2));  //WPG01 = WPG01
     }
+
+    @Test
+    public void testContains() {
+        assertFalse(AIRPORT1.contains(null));
+        assertFalse(AIRPORT1.contains("ASDJIASJ"));
+        assertFalse(AIRPORT1.contains(airport2));
+        Airport match = new Airport(CODE1);
+        assertTrue(AIRPORT1.contains(match));                // Exact match should return true
+        Airport subset = new Airport(CODE1.substring(0, 2));
+        assertTrue(AIRPORT1.contains(subset));               // Subset match should return true
+    }
+
+
 
 }
