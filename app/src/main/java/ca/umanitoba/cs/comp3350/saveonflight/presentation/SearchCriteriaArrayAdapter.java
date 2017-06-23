d571d9d94c4164bef5e4ff32c7e2c295e37416d2
Kenny Zhang <busstopbench@gmail.com>
2017-06-23 15:48:56 -0500

    Don't let users pick dates before the current date when searching for flights

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/presentation/SearchCriteriaArrayAdapter.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/presentation/SearchCriteriaArrayAdapter.java
@@ -145,6 +145,7 @@ public class SearchCriteriaArrayAdapter extends ArrayAdapter<SearchCriteriaListV
     private void showDatePickerDialog() {
         Calendar calendar = Calendar.getInstance();
         DatePickerDialog dialog = new DatePickerDialog(this.getContext(), this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        dialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
         dialog.show();
     }
 
