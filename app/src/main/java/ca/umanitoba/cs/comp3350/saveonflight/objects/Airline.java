04455bc06247d9d15a787719b8f38713f57c5820
Andy Lun <andylun1206@gmail.com>
2017-06-28 23:23:50 -0500

    Removing parcelable from airline

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/objects/Airline.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/objects/Airline.java
@@ -12,7 +12,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
-public class Airline implements Parcelable, Comparable  {
+public class Airline implements Comparable  {
     private String name;
     private int icon;
 
@@ -46,32 +46,6 @@ public class Airline implements Parcelable, Comparable  {
         }
 
         return result;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel parcel, int flags) {
-        parcel.writeString(name);
-    }
-
-    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
-        @Override
-        public Airline[] newArray(int size) {
-            return new Airline[size];
-        }
-
-        @Override
-        public Airline createFromParcel(Parcel in) {
-            return new Airline(in);
-        }
-    };
-
-    private Airline(Parcel in) {
-        name = in.readString();
     }
 
     @Override
