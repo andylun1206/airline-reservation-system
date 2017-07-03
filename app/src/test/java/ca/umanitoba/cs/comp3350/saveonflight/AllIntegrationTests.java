1d3440c7e6544ae2d462ddf7ee95c147b3e60d23
Kenny Zhang <busstopbench@gmail.com>
2017-07-03 12:50:58 -0500

    Add to searchByTraveller test

--- app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/AllIntegrationTests.java
+++ app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/AllIntegrationTests.java
@@ -1,8 +1,10 @@
-package ca.umanitoba.cs.comp3350.saveonflight.integration;
+package ca.umanitoba.cs.comp3350.saveonflight;
 
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+import ca.umanitoba.cs.comp3350.saveonflight.integration.BookedFlightsBusinessPersistenceSeamTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
--- app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/integration/BookedFlightsBusinessPersistenceSeamTest.java
+++ app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/integration/BookedFlightsBusinessPersistenceSeamTest.java
@@ -2,11 +2,8 @@ package ca.umanitoba.cs.comp3350.saveonflight.integration;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import ca.umanitoba.cs.comp3350.saveonflight.application.Main;
@@ -18,47 +15,47 @@ import ca.umanitoba.cs.comp3350.saveonflight.objects.Traveller;
 import static org.junit.Assert.*;
 
 public class BookedFlightsBusinessPersistenceSeamTest {
-    private static AccessBookedFlights access;
+    private static AccessBookedFlights accessBf;
 
     @BeforeClass
     public static void setUp() {
         Main.startUp(Main.DatabaseType.HSQL);
-        access = new AccessBookedFlightsImpl(Main.getBookedFlightAccess());
+        accessBf = new AccessBookedFlightsImpl(Main.getBookedFlightAccess());
     }
 
     @AfterClass
     public static void tearDown() {
         Main.shutDown();
-        access = null;
+        accessBf = null;
     }
 
     @Test
     public void testAddAndRemove() {
-        BookedFlight bf = access.searchByTraveller(new Traveller(1, "name doesn't matter")).get(0);
+        BookedFlight bf = accessBf.searchByTraveller(new Traveller(1, "name doesn't matter")).get(0);
 
         // Try to adding null
-        assertFalse(access.add(null));
+        assertFalse(accessBf.add(null));
 
         // Add new row to table
         bf.getFlight().setFlightCode("WJ 123");
-        assertTrue(access.add(bf));
+        assertTrue(accessBf.add(bf));
 
         // Add another row
         bf.getFlight().setFlightCode("AC 123");
-        assertTrue(access.add(bf));
+        assertTrue(accessBf.add(bf));
 
         // Remove the row just added
-        assertTrue(access.remove(bf));
+        assertTrue(accessBf.remove(bf));
 
         // Remove the first row that was added
         bf.getFlight().setFlightCode("WJ 123");
-        assertTrue(access.remove(bf));
+        assertTrue(accessBf.remove(bf));
 
         // Try to remove a row not in the table
-        assertFalse(access.remove(bf));
+        assertFalse(accessBf.remove(bf));
 
         // Try to remove null
-        assertFalse(access.remove(null));
+        assertFalse(accessBf.remove(null));
     }
 
     @Test
@@ -66,13 +63,22 @@ public class BookedFlightsBusinessPersistenceSeamTest {
         // The default data in the database includes BookedFlights for passengers with id's 0, 1, and 2
         for (int i = 0; i < 3; i++) {
             Traveller t = new Traveller(i, "name doesn't matter");
-            List<BookedFlight> bfs = access.searchByTraveller(t);
+            List<BookedFlight> bfs = accessBf.searchByTraveller(t);
 
             assertNotNull(bfs);
             for (BookedFlight bf : bfs) {
                 assertNotNull(bf.getTraveller());
                 assertEquals(t, bf.getTraveller());
             }
+        }
+
+        // These searches should give empty results
+        for (int i = 4; i < 100; i++) {
+            Traveller t = new Traveller(i, "name doesn't matter");
+            List<BookedFlight> bfs = accessBf.searchByTraveller(t);
+
+            assertNotNull(bfs);
+            assertTrue(bfs.isEmpty());
         }
     }
 }
