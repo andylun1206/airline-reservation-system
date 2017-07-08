4661c064afb670185fbc16b6e06d888af754784c
Kenny Zhang <zhang-youtian@hotmail.com>
2017-07-08 13:34:34 -0500

    Add tests for getAirlines and getAirlineByName

--- app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/AllIntegrationTests.java
+++ app/src/test/java/ca/umanitoba/cs/comp3350/saveonflight/AllIntegrationTests.java
@@ -4,6 +4,7 @@ package ca.umanitoba.cs.comp3350.saveonflight;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import ca.umanitoba.cs.comp3350.saveonflight.integration.AirlineBusinessPersistenceSeamTest;
 import ca.umanitoba.cs.comp3350.saveonflight.integration.AirportsBusinessPersistenceSeamTest;
 import ca.umanitoba.cs.comp3350.saveonflight.integration.BookedFlightsBusinessPersistenceSeamTest;
 import ca.umanitoba.cs.comp3350.saveonflight.integration.TravellerBusinessPersistenceSeamTest;
@@ -12,7 +13,8 @@ import ca.umanitoba.cs.comp3350.saveonflight.integration.TravellerBusinessPersis
 @Suite.SuiteClasses({
         BookedFlightsBusinessPersistenceSeamTest.class,
         TravellerBusinessPersistenceSeamTest.class,
-        AirportsBusinessPersistenceSeamTest.class
+        AirportsBusinessPersistenceSeamTest.class,
+        AirlineBusinessPersistenceSeamTest.class
 })
 
 public class AllIntegrationTests {
