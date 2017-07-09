8fe1dd7491c0d59174b1638c3bcc2ed24aaf3e48
Kenny Zhang <busstopbench@gmail.com>
2017-07-09 17:26:59 -0500

    Add acceptance test for ticket page

--- app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/presentation/MainActivity.java
+++ app/src/main/java/ca/umanitoba/cs/comp3350/saveonflight/presentation/MainActivity.java
@@ -87,8 +87,10 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
 
             if (f instanceof ViewFlightsFragment || f instanceof PaymentFragment || f instanceof ViewFlightsSummaryFragment) {
                 displaySelectedScreen(R.id.nav_search);
-            } else if (f instanceof SearchFragment || f instanceof ViewBookedFlightFragment || f instanceof TicketFragment) {
+            } else if (f instanceof SearchFragment || f instanceof ViewBookedFlightFragment) {
                 displaySelectedScreen(R.id.nav_home);
+            } else if (f instanceof TicketFragment) {
+                displaySelectedScreen(R.id.nav_bookedflights);
             } else {
                 super.onBackPressed();
             }
