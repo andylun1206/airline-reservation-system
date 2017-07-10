0f6975bc92941c631581d39fc9bce4f6c95a788c
Shenyun Wang <wangs321@myumanitoba.ca>
2017-07-10 17:36:19 -0500

    roundtrip and one way flight search testing

--- app/src/androidTest/java/ca/umanitoba/cs/comp3350/saveonflight/acceptance/SearchTest.java
+++ app/src/androidTest/java/ca/umanitoba/cs/comp3350/saveonflight/acceptance/SearchTest.java
@@ -32,10 +32,15 @@ public class SearchTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public void testRoundTrip(){
         solo.waitForActivity("MainActivity");
         solo.clickOnButton("SEARCH FOR FLIGHTS");
-        Assert.assertTrue(solo.searchText("2017-11-11"));
-        Assert.assertTrue(solo.searchText("2017-12-11"));
-        Assert.assertTrue(solo.searchText("Winnipeg YWG"));
-        Assert.assertTrue(solo.searchText("Toronto YYZ"));
+        solo.enterText(0, "Winnipeg YWG");
+        solo.enterText(1, "Toronto YYZ");
+        solo.clickOnText("Departure Date");
+        solo.setDatePicker(0, 2017, 10, 11); // Passing a month of 10 maps to November (11th month)?
+        solo.clickOnText("OK");
+        solo.clickOnText("Return Date");
+        solo.setDatePicker(0, 2017, 11, 11); // And passing a month of 11 maps to December???
+        solo.clickOnText("OK");
+
         solo.clickOnButton("Search");
         solo.clickOnText("AC 266");
         solo.clickOnText("AC 101");
@@ -51,12 +56,41 @@ public class SearchTest extends ActivityInstrumentationTestCase2<MainActivity> {
         solo.clickOnButton("SEARCH FOR FLIGHTS");
         solo.clickOnText("Return");
         solo.clickOnText("One Way");
+        solo.enterText(0, "Winnipeg YWG");
+        solo.enterText(1, "Toronto YYZ");
+        solo.clickOnText("Departure Date");
+        solo.setDatePicker(0, 2017, 10, 11);
+        solo.clickOnText("OK");
+        solo.clickOnButton("Search");
+        solo.clickOnText("AC 260");
+
         Assert.assertTrue(solo.searchText("2017-11-11"));
         Assert.assertTrue(solo.searchText("2017-12-11"));
         Assert.assertTrue(solo.searchText("Winnipeg YWG"));
         Assert.assertTrue(solo.searchText("Toronto YYZ"));
-        solo.clickOnButton("Search");
-        solo.clickOnText("AC 206");
+        Assert.assertTrue(solo.searchText("AC 260"));
 
     }
+
+//    public void testAdvancedSearch(){
+//        solo.waitForActivity("MainActivity");
+//        solo.clickOnButton("SEARCH FOR FLIGHTS");
+//        solo.clickOnText("ADVANCED OPTIONS");
+//        solo.enterText(0, "Winnipeg YWG");
+//        solo.enterText(1, "Toronto YYZ");
+//        solo.clickOnText("Departure Date");
+//        solo.setDatePicker(0, 2017, 10, 11);
+//        solo.clickOnText("OK");
+//        solo.clickOnText("Return Date");
+//        solo.setDatePicker(1,2017,11,11);
+//        solo.clickOnText("OK");
+//        solo.clickOnText("Maximum Price");
+//        solo.enterText(2,"300");
+//        solo.clickOnText("Preferred Airlines");
+//        solo.enterText(3,"West Jet");
+//        solo.clickOnText("Preferred Class");
+//        solo.enterText(4,"Economy Class");
+//        solo.clickOnButton("SEARCH");
+//
+//    }
 }
