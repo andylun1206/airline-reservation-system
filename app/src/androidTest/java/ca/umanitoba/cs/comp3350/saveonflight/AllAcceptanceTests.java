c95df2874ed05fa055375d309ffe3f7c598100b4
Kenny Zhang <busstopbench@gmail.com>
2017-07-10 12:59:04 -0500

    Add acceptnace test for view flights screen

--- app/src/androidTest/java/ca/umanitoba/cs/comp3350/saveonflight/AllAcceptanceTests.java
+++ app/src/androidTest/java/ca/umanitoba/cs/comp3350/saveonflight/AllAcceptanceTests.java
@@ -6,6 +6,7 @@ import junit.framework.TestSuite;
 import ca.umanitoba.cs.comp3350.saveonflight.acceptance.PaymentTest;
 import ca.umanitoba.cs.comp3350.saveonflight.acceptance.TicketTest;
 import ca.umanitoba.cs.comp3350.saveonflight.acceptance.ViewBookedFlightsTest;
+import ca.umanitoba.cs.comp3350.saveonflight.acceptance.ViewFlightsTest;
 
 public class AllAcceptanceTests {
     public static TestSuite suite;
@@ -15,6 +16,7 @@ public class AllAcceptanceTests {
         suite.addTestSuite(ViewBookedFlightsTest.class);
         suite.addTestSuite(PaymentTest.class);
         suite.addTestSuite(TicketTest.class);
+        suite.addTestSuite(ViewFlightsTest.class);
         return suite;
     }
 }
